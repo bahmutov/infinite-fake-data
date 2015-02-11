240f937ef35e3e7d8c5e7fba5493e7c0bab857a4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-02-11 09:19:33 -0500

    generated

--- dist/src/app.js
+++ dist/src/app.js
@@ -40,33 +40,29 @@ angular.module("tester", ["infinite-fake-data", "ngMockE2E", "chieffancypants.lo
 },{"./slow-down-http.es6":3}],2:[function(require,module,exports){
 "use strict";
 
-(function (angular) {
-  "use strict";
+function infiniteFakeDataCtrl($scope, $http) {
+  $scope.fetching = false;
+  $scope.people = [];
 
-  function infiniteFakeDataCtrl($scope, $http) {
-    $scope.fetching = false;
-    $scope.people = [];
+  var startIndex = 0;
+  var fetchNumber = 50;
 
-    var startIndex = 0;
-    var fetchNumber = 50;
+  $scope.fetch = function () {
+    $scope.fetching = true;
 
-    $scope.fetch = function () {
-      $scope.fetching = true;
+    $http.get("/people/slice/" + startIndex + "/" + (startIndex + fetchNumber)).success(function (response) {
+      $scope.people = $scope.people.concat(response);
+      if (response.length) {
+        startIndex += response.length;
+      }
+    })["finally"](function () {
+      $scope.fetching = false;
+    });
+  };
+  $scope.fetch();
+}
 
-      $http.get("/people/slice/" + startIndex + "/" + (startIndex + fetchNumber)).success(function (response) {
-        $scope.people = $scope.people.concat(response);
-        if (response.length) {
-          startIndex += response.length;
-        }
-      })["finally"](function () {
-        $scope.fetching = false;
-      });
-    };
-    $scope.fetch();
-  }
-
-  angular.module("infinite-fake-data", ["infinite-scroll"]).controller("infiniteFakeDataCtrl", infiniteFakeDataCtrl);
-})(angular);
+angular.module("infinite-fake-data", ["infinite-scroll"]).controller("infiniteFakeDataCtrl", infiniteFakeDataCtrl);
 
 },{}],3:[function(require,module,exports){
 "use strict";
